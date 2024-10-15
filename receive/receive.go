14c7997962d976d99f78297f5cc1074b48fdd814
Naveen Kumar Sangi <nkprince007@users.noreply.github.com>
2024-10-15 11:32:43 +0530

    Update receive.go
    Signed-off-by: Naveen Kumar Sangi <nkprince007@users.noreply.github.com>

--- receive/receive.go
+++ receive/receive.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"sync/atomic"
 	"time"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -12,6 +16,8 @@ func failOnError(err error, msg string) {
 		log.Fatalf("%s: %s", msg, err)
 	}
 }
+
+var messageCount uint64 // Counter to keep track of the number of processed messages
 
 func main() {
 	url := os.Args[1]
@@ -22,8 +28,6 @@ func main() {
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
-
-	failOnError(err, "Failed to set QoS")
 
 	q, err := ch.QueueDeclare(
 		"hello", // name
@@ -40,6 +44,7 @@ func main() {
 		0,     // prefetch size
 		false, // global
 	)
+	failOnError(err, "Failed to set QoS")
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -54,11 +59,25 @@ func main() {
 
 	forever := make(chan bool)
 
+	// Start HTTP server in a separate goroutine
+	go func() {
+		http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+			count := atomic.LoadUint64(&messageCount)
+			fmt.Fprintf(w, "Messages processed: %d\n", count)
+		})
+		log.Println("Starting HTTP server on :8080")
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	// Process messages in a separate goroutine
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
-			time.Sleep(1 * time.Second)
+			time.Sleep(1 * time.Second) // Simulating work
 			d.Ack(false)
+
+			// Increment the message count safely
+			atomic.AddUint64(&messageCount, 1)
 		}
 	}()
 
